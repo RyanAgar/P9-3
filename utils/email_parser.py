4e5780bcec946bbc77cc23a1ffa3226247883fc5
gongctf <113359531+gongctf@users.noreply.github.com>
2025-09-18 18:47:40 +0000

    Email Parser Function
    Accepts:
    Raw Email (str)
    
    Returns:
    Sender (str)
    Subject (str)
    Body (str)
    Urls (list)

---
+++ utils/email_parser.py
@@ -0,0 +1,20 @@
+import re
+from email import message_from_string
+
+def parse_email(raw_email):
+    message = message_from_string(raw_email)
+    sender = message.get("From","")
+    subject = message.get("Subject","")
+    
+    #Get body (check if email is multipart -> get body from text/plain type)
+    body = ""
+    if message.is_multipart():
+        for part in message.walk():
+            if part.get_content_type() == "text/plain":
+                body += part.get_payload(decode=True).decode(errors="ignore")
+
+    else:
+        body = message.get_payload(decode=True).decode(errors="ignore")
+    
+    urls = re.findall(r'(https?://\S+)', body)
+    return sender, subject, body, urls

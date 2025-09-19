6b66d56c0e8029da5369af7354220678c1f4a26f
Chong Min Han <chongmh18@gmail.com>
2025-09-20 01:13:37 +0800

    Add dataset handling script

---
+++ utils/dataset_loader.py
@@ -0,0 +1,12 @@
+import os
+
+def load_dataset(path):
+    emails = []
+    labels = []
+    for label in ["spam", "ham"]: # Loops through "spam" and "ham" folders
+        folder = os.path.join(path, label)
+        for filename in os.listdir(folder): #Read each file in the folder
+            with open(os.path.join(folder, filename), "r", encoding="latin-1") as f:
+                emails.append(f.read()) # Add email content to list
+                labels.append(label) # Add corresponding label
+    return emails, labels

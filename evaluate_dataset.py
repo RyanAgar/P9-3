84f489ef3b427d6b3010dd22296ad750e2e845a2
Ryan Ng <ryanng6948@gmail.com>
2025-09-23 12:06:12 +0800

    fixed bug in evaluating dataset

--- evaluate_dataset.py
+++ evaluate_dataset.py
@@ -10,7 +10,8 @@ def evaluate(path):
     for email, label in zip(emails, labels):
         sender, subject, body, urls = parse_email(email)
         prediction = final_score(sender, subject, body, urls)
-        predicted_label = prediction.lower()
+        predicted_label, score = final_score(sender, subject, body, urls)
+        predicted_label = predicted_label.lower()
         true_label = "spam" if label == "spam" else "safe"
 
         if predicted_label == true_label:
